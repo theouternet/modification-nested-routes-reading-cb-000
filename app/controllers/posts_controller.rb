4eed2ce83c03e9eed98181d6b392e5d6e536780f
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-05-04 06:14:42 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,10 +16,13 @@ class PostsController < ApplicationController
     end
   end
 
-   def new
-  @post = Post.new(author_id: params[:author_id])
-end
+  def new
+  if params[:author_id] && !Author.exists?(params[:author_id])
+    redirect_to authors_path, alert: "Author not found."
+  else
+    @post = Post.new(author_id: params[:author_id])
   end
+end
 
   def create
     @post = Post.new(post_params)
@@ -33,9 +36,19 @@ end
     redirect_to post_path(@post)
   end
 
-  def edit
+ def edit
+  if params[:author_id]
+    author = Author.find_by(id: params[:author_id])
+    if author.nil?
+      redirect_to authors_path, alert: "Author not found."
+    else
+      @post = author.posts.find_by(id: params[:id])
+      redirect_to author_posts_path(author), alert: "Post not found." if @post.nil?
+    end
+  else
     @post = Post.find(params[:id])
   end
+end
 
   private
 
